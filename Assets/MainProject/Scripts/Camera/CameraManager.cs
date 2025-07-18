30cf3fab065b5d0fae32207afa5f0616110e352f
Aftab-Home <aftab.pilover@gmail.com>
2025-07-19 02:12:36 +0600

    CameraManager and CamShake scripts added

---
+++ Assets/MainProject/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Aftab
+{
+    public class CameraManager : MonoBehaviour
+    {
+        public static CameraManager Instance { get; private set; }
+        public Camera MainCam { get; private set; }
+        [SerializeField]
+        bool shouldCallManageCameraRatio = true;
+        const float longDeviceSizeMultiplier = 1.04f;
+        const float _refSR = (float)1920 / 1080;
+        float screenRatio;
+
+        void Awake()
+        {
+            Instance = this;
+            MainCam = GetComponent<Camera>();
+            ManageCameraRatio();
+        }
+
+        /// <summary>
+        /// This will manage the camera fov or orthographic size according to device screen ratio
+        /// </summary>
+        void ManageCameraRatio()
+        {
+            if (!shouldCallManageCameraRatio) return;
+            screenRatio = (float)Screen.height / Screen.width;
+            //If screen ratio is less than 1.78f
+            //if(screenRatio <= 1.78f) return;
+            //If this camera is not orthographic
+            if (!MainCam.orthographic) MainCam.fieldOfView = (MainCam.fieldOfView * screenRatio * longDeviceSizeMultiplier) / _refSR;
+            else MainCam.orthographicSize = (MainCam.orthographicSize / _refSR) * screenRatio;
+        }
+    }
+}
+
---
+++ Assets/MainProject/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Aftab
+{
+    public class CameraShake : MonoBehaviour
+    {
+        public static CameraShake Instance { get; private set; }
+
+        Coroutine runningCoroutine;
+
+        void Awake() => Instance = this;
+
+        public void ShakeTheCamera(float _duration, float _magnitude)
+        {
+            if (runningCoroutine != null) StopCoroutine(runningCoroutine);
+            runningCoroutine = StartCoroutine(Shake(_duration, _magnitude));
+        }
+
+        IEnumerator Shake(float _duration, float _magnitude)
+        {
+            Vector3 originalPos = transform.localPosition;
+
+            float elapsedTime = 0.0f;
+
+            while (elapsedTime < _duration)
+            {
+                float x = originalPos.x + Random.Range(-1f, 1f) * _magnitude;
+                float y = originalPos.y + Random.Range(-1f, 1f) * _magnitude;
+                float z = originalPos.z + Random.Range(-1f, 1f) * _magnitude;
+
+                transform.localPosition = new Vector3(x, y, originalPos.z);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.localPosition = originalPos;
+        }
+    }
+}
+
