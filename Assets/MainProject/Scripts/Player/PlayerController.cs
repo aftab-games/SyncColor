258739e5a7acc1512a09b5da0f521ff2f854f06d
Aftab-Home <aftab.pilover@gmail.com>
2025-07-20 01:14:33 +0600

    Side movement cont.

--- Assets/MainProject/Scripts/Player/PlayerController.cs
+++ Assets/MainProject/Scripts/Player/PlayerController.cs
@@ -8,7 +8,14 @@ namespace Aftab
         [SerializeField]
         float _forwardSpeed = 1.0f;
         [SerializeField]
-        float _sideSpeed = 1.0f;
+        float _sideSpeed = 10.0f;
+
+        float _laneDistance = 2.5f;
+
+        int _currentLane = 1; // 0 = Left, 1 = Center, 2 = Right
+
+        Vector3 _targetPosition = Vector3.zero;
+
         [SerializeField]
         bool _canMove = false;
         Rigidbody _rb;
@@ -20,6 +27,7 @@ namespace Aftab
         }
         void Update()
         {
+            ManageInput();
             ManageSideWiseMovement();
         }
 
@@ -37,9 +45,34 @@ namespace Aftab
             _rb.linearVelocity = _targetVelocity;
         }
 
+        void ManageInput()
+        {
+            // OLD INPUT SYSTEM: Will Replace with New Input System later
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            {
+                ChangeLane(-1);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            {
+                ChangeLane(1);
+            }
+        }
+
+        private void ChangeLane(int direction)
+        {
+            _currentLane = Mathf.Clamp(_currentLane + direction, 0, 2);
+            float newX = (_currentLane - 1) * _laneDistance;
+            _targetPosition = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+
+
         void ManageSideWiseMovement()
         {
             if (!_canMove) return;
+            Vector3 newPosition = Vector3.Lerp(transform.position, 
+                new Vector3(_targetPosition.x, transform.position.y, transform.position.z),
+                Time.deltaTime * _sideSpeed);
+            transform.position = newPosition;
         }
     }
 }
