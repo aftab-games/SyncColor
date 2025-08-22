ae70fafbb97b8ea046f2ace4107c5fe6f9a18813
Aftab-Games-Home <aftab.uddin.games@gmail.com>
2025-08-22 09:28:05 +0600

    UiManager script updated

--- Assets/MainProject/Scripts/Common/UiManager.cs
+++ Assets/MainProject/Scripts/Common/UiManager.cs
@@ -1,19 +1,48 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Aftab
 {
     public class UiManager : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        [Header("Settings:")]
+        [SerializeField]
+        GameObject lvlCompletePanelGO, lvlFailedPanelGO;
+        [SerializeField]
+        Button startGameBtn, nextLvlBtn, retryBtn;
+
+        public static UiManager Instance {  get; private set; }
+
+        void Awake()
         {
-        
+            Instance = this;
         }
 
-        // Update is called once per frame
-        void Update()
+        void Start()
         {
-        
+            startGameBtn.onClick.AddListener(ManageTappedStartGameBtn);
+            nextLvlBtn.onClick.AddListener (ManageTappedNextLvlBtn);
+            retryBtn.onClick.AddListener (ManageTappedRetryBtn);
+        }
+
+        void OnDisable()
+        {
+            startGameBtn.onClick.RemoveAllListeners();
+            nextLvlBtn.onClick.RemoveAllListeners();
+            retryBtn.onClick.RemoveAllListeners();
+        }
+        void ManageTappedStartGameBtn()
+        {
+
+        }
+        void ManageTappedNextLvlBtn()
+        {
+
+        }
+        void ManageTappedRetryBtn()
+        {
+
         }
     }
 }
