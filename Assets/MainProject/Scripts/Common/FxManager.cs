a8fba23812ba687a5c7cffefddf68b0925c3b59f
Aftab-Games-Home <aftab.uddin.games@gmail.com>
2025-08-22 23:28:54 +0600

    FxManager added

---
+++ Assets/MainProject/Scripts/Common/FxManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace Aftab
+{
+    public class FxManager : MonoBehaviour
+    {
+        [Header("Settings:")]
+        [SerializeField]
+        ParticleSystem winFx;
+        [SerializeField]
+        ParticleSystem ballBreakFx;
+        void Start()
+        {
+            GameManager.Instance.OnLvlCompleted += ManageLevelCompleted;
+            GameManager.Instance.OnLvlFailed += PlayBallBreakFx;
+        }
+
+        void OnDisable()
+        {
+            GameManager.Instance.OnLvlCompleted -= ManageLevelCompleted;
+            GameManager.Instance.OnLvlFailed -= PlayBallBreakFx;
+        }
+        void ManageLevelCompleted()
+        {
+            winFx.transform.position = LevelEndSystem.Instance.transform.position + new Vector3(0, 8, 5);
+            winFx.gameObject.SetActive(true);
+            Invoke(nameof(PlayWinFx), 0.1f);
+        }
+
+        void PlayWinFx()
+        {
+            winFx.Play();
+        }
+
+        void PlayBallBreakFx()
+        {
+            ballBreakFx.transform.position = PlayerController.Instance.transform.position;
+            ballBreakFx.gameObject.SetActive(true);
+            ballBreakFx.Play();
+        }
+    }
+}
