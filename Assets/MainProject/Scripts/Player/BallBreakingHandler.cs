ee2e323aefa5ef468585ae00da1163e6b36e9826
Aftab-Games-Home <aftab.uddin.games@gmail.com>
2025-08-25 07:42:36 +0600

    Ball breaking finish at the moment

--- Assets/MainProject/Scripts/Player/BallBreakingHandler.cs
+++ Assets/MainProject/Scripts/Player/BallBreakingHandler.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Aftab
@@ -8,14 +10,35 @@ namespace Aftab
         [SerializeField]
         GameObject brokenPiecesParentGO;
 
-        void ManageBallBreak()
+        public void ManageBallBreak()
         {
             //TODO: Instantiate prefab
+            Transform brokenBallTr = Instantiate(brokenPiecesParentGO, transform).transform;
             //TODO: Place inside the player controller. I mean make the instantiated object as a child of this object
+            brokenBallTr.SetParent(transform);
+            Transform ballMainBodyTr = PlayerController.Instance.GetBallTransform();
+            
+            brokenBallTr.localPosition = ballMainBodyTr.localPosition;
+            brokenBallTr.localRotation = ballMainBodyTr.localRotation;
+            brokenBallTr.localScale = ballMainBodyTr.localScale;
+            //TODO: Get currentballColor
+            Color currentBallColor = PlayerController.Instance.GetPlayerColor();
+            //TODO: Deactivate Ball Main Transform
+            ballMainBodyTr.gameObject.SetActive(false);
             //TODO: Get all the mesh renderer
             //TODO: Assign current ball color to all the renderer
             //TODO: Get all the rigidbody insided
             //TODO: Give an explosion force or custom force
+            //List<Transform> childTransforms = brokenBallTr.GetComponentsInChildren<Transform>().ToList<Transform>();
+            foreach (Transform childTr in brokenBallTr)
+            {
+                Debug.Log(childTr.name);
+                MeshRenderer meshRenderer = childTr.GetComponent<MeshRenderer>();
+                meshRenderer.material.color = currentBallColor;
+                Rigidbody rigidbody = childTr.GetComponent<Rigidbody>();
+                //rigidbody.AddExplosionForce(5000, brokenBallTr.position, 20, 20, ForceMode.VelocityChange);
+                rigidbody.linearVelocity = new Vector3(Random.Range(-5, 5), Random.Range(3, 5), Random.Range(-5, 5));
+            }
         }
     }
 }
